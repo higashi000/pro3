8035e68f0d998d8ca87e614324b0893a7262b087
higashi000 <higashi.ict@gmail.com>
2020-01-07 20:50:48 +0900

    Add separate card

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-def hand_delete(hand):
-    new_hand = []
-    hand.sort()
+import random
+import player
 
-    first_hand = ""
-    second_hand = ""
-    first_hand = hand.pop()
-    second_hand = hand.pop()
+users = []
+client_list = []
 
-    while len(hand) > 0:
-        if first_hand != second_hand:
-            new_hand.append(first_hand)
-            first_hand = second_hand
-            second_hand = hand.pop()
-        else:
-            first_hand = hand.pop()
-            second_hand = hand.pop()
+card = ["01a", "02a", "03a", "04a", "05a", "06a", "07a", "08a", "09a", "10a", "11a", "12a", "13a",
+        "01s", "02s", "03s", "04s", "05s", "06s", "07s", "08s", "09s", "10s", "11s", "12s", "13s",
+        "01d", "02d", "03d", "04d", "05d", "06d", "07d", "08d", "09d", "10d", "11d", "12d", "13d",
+        "01h", "02h", "03h", "04h", "05h", "06h", "07h", "08h", "09h", "10h", "11h", "12h", "13h",
+        "00j"]
 
-    return new_hand
+def separate_card():
+    random.shuffle(card)
+    handover_card = [card[0:13], card[13:26], card[26:39], card[39:]]
+    random.shuffle(handover_card)
 
-def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	server.send_message_to_all("Hey all, a new client has joined us")
+    for i in handover_card:
+        print(len(i))
 
-
-def client_left(client, server):
-	print("Client(%d) disconnected" % client['id'])
-
-
-def message_received(client, server, message):
-	if len(message) > 200:
-		message = message[:200]+'..'
-    print("Client(%d) said: %s" % (client['id'], message))
-
-PORT=9001
-server = WebsocketServer(PORT)
-server.set_fn_new_client(new_client)
-server.set_fn_client_left(client_left)
-server.set_fn_message_received(message_received)
-server.run_forever()
+    users.append(player.Player(handover_card[0], client_list[0], client_list[1], client_list[3]))
+    users.append(player.Player(handover_card[1], client_list[1], client_list[2], client_list[0]))
+    users.append(player.Player(handover_card[2], client_list[2], client_list[3], client_list[1]))
+    users.append(player.Player(handover_card[3], client_list[3], client_list[0], client_list[2]))
