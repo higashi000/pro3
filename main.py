523951fbe535cc1ed3e597e08a5c20eaed2a2fed
higashi000 <higashi.ict@gmail.com>
2020-01-08 18:57:52 +0900

    Add websocket server

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from websocket_server import WebsocketServer
 import random
 import player
 
 users = []
 client_list = []
+now_player = ""
 
 card = ["01a", "02a", "03a", "04a", "05a", "06a", "07a", "08a", "09a", "10a", "11a", "12a", "13a",
         "01s", "02s", "03s", "04s", "05s", "06s", "07s", "08s", "09s", "10s", "11s", "12s", "13s",
@@ -22,3 +24,41 @@ def separate_card():
     users.append(player.Player(handover_card[1], client_list[1], client_list[2], client_list[0]))
     users.append(player.Player(handover_card[2], client_list[2], client_list[3], client_list[1]))
     users.append(player.Player(handover_card[3], client_list[3], client_list[0], client_list[2]))
+
+def game_start(server):
+    now_player = users[0].my_client
+
+    for i in users:
+        server.send_message(i.my_client, i.hand_json)
+
+def update_order():
+    for i in users:
+        if i.my_client['id'] == now_player:
+            now_player = i.next_user['id']
+
+def new_client(client, server):
+    if len(client_list) < 4:
+        client_list.append(client)
+        server.send_message(client, "aaaa")
+        print("Join new user")
+    else:
+        print("over capacity")
+        server.send_message(client, "{\"status\":\"false\",\"message\":\"Sorry over capacity\"}")
+        return
+
+    if len(client_list) == 4:
+        separate_card()
+        game_start(server)
+
+def message_received(client, server, message):
+    if len(message) > 200:
+        message = message[:200]+'..'
+
+    print("Client(%d) said: %s" % (client['id'], message))
+
+PORT = 9001
+server = WebsocketServer(PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_list)
+server.set_fn_message_received(message_received)
+server.run_forever()
